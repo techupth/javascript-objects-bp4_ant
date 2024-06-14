45a2af3cd11828302608802bd44bb1cdd30a2791
konthron gongmanee <konthron.ant@gmail.com>
2024-06-14 18:56:23 +0700

    feat:Object assignment finish

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,13 @@
 // Exercise #1: Food Order
 
 // Start coding here
+const foodOrder = {
+    orderNumber: "A0234",
+    address: "Bangkok",
+    paymentType: "cash",
+    totalPrice: 4500,
+    retaurantName: "MK"
+}
+foodOrder["totalPrice"] = 5000
+foodOrder.paymentType = "Creditcard"
+console.log(foodOrder)
